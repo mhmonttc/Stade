86b6f3f09bd7f2330f46d604708c01cae2b1be58
mhmc <mhmonttc@gmail.com>
2023-09-24 16:01:36 -0300

    Agrega boton de pruebas en ActivityMain, además de agregar compatibilidad con lectura de QR y EAN

--- app/src/main/java/com/example/appembalaje/MainActivity.java
+++ app/src/main/java/com/example/appembalaje/MainActivity.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
+
+import com.example.appembalaje.adaptadores.QrHanlder;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +22,24 @@ public class MainActivity extends AppCompatActivity {
     public void ingresar(View view){
         Intent login = new Intent(this, ingresar.class);
         startActivity(login);
+    }
+
+    public void PruebasScanner(View view){
+        QrHanlder scan = new QrHanlder();
+        scan.ReadCode(MainActivity.this,2);
+    }
+
+    protected void onActivityResult(int RequestCode,int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(RequestCode,resultCode,data);
+        if(result !=null){
+            if(result.getContents()==null){
+                Toast.makeText(this, "Lector Cerrado antes de tiempo", Toast.LENGTH_SHORT).show();
+            }else {
+                Toast.makeText(this, result.getContents(), Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            super.onActivityResult(RequestCode, resultCode, data);
+        }
+
     }
 }
--- app/src/main/java/com/example/appembalaje/adaptadores/QrHanlder.java
+++ app/src/main/java/com/example/appembalaje/adaptadores/QrHanlder.java
@@ -1,14 +1,23 @@
 package com.example.appembalaje.adaptadores;
 
+import android.app.Activity;
+import com.google.zxing.integration.android.IntentIntegrator;
+
 public class QrHanlder {
 
-    public int ReadEANCode(){
-
-        return 0;
+    public String ReadCode(Activity activity,int tipoLector){
+        String codigo = (tipoLector==1) ? IntentIntegrator.QR_CODE:IntentIntegrator.EAN_13;
+        Boolean beep= (tipoLector==1) ? Boolean.FALSE:Boolean.TRUE;
+        IntentIntegrator integrador = new IntentIntegrator(activity);
+        integrador.setDesiredBarcodeFormats(codigo);
+        integrador.setCameraId(0);
+        integrador.setBeepEnabled(beep);
+        integrador.setBarcodeImageEnabled(true);
+        integrador.initiateScan();
+        return null;
     }
 
-    public String ReadQRCode(){
-
+    public String GenerateQrCode(String data){
         return null;
     }
 
